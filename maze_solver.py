6e85866f8acd7879a52e2458ed1c5032cb05423c
Lanting Chiang <49918461+lantingchiang@users.noreply.github.com>
2020-04-20 23:16:40 -0400

    started skeleton file

---
+++ maze_solver.py
@@ -0,0 +1,41 @@
+
+def parse_args():
+    """
+    Parses commane line arguments with argparse
+    """
+    pass
+
+
+def construct_maze():
+    """
+    constructs matrix from data read in 
+    """
+    pass
+
+
+def solve_maze():
+    """
+    solves maze recursively
+    """
+    pass
+
+
+def draw_soluion():
+    """
+    draws out solution maze using pyplot
+    """
+    pass
+
+
+def print_solution():
+    """
+    prints out coordinates of solution path in command line
+    """
+    pass
+
+
+def generate_new_maze():
+    """
+    generate a new maze to output to the user
+    """
+    pass
