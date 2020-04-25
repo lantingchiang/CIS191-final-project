a4c0d7e8680256faa347d9bc87530ff8d82a24cb
ph163k8 <30556772+ph163k8@users.noreply.github.com>
2020-04-25 04:56:32 -0400

    Add files via upload

--- maze_solver.py
+++ maze_solver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+from graph import Graph 
 
 
 def parse_args():
@@ -34,21 +35,94 @@ def parse_args():
     return args
 
 
-def construct_maze():
+def construct_maze(args):
     """
     constructs matrix from data read in 
+    -----------
+    Parameters:
+    args: arguments parsed from parser
+    -----------
+    Returns:
+    2d array representation of maze
     """
-    pass
+    matrix = []
+
+    # length of first row (all rows should have the same length)
+    length = len(args.maze_rows[0])
+
+    # check for src and tgt in bounds
+    min = min(args.src_x, args.src_y, args.tgt_x, args.tgt_y)
+    max_x = max(args.src_x, args.tgt_x)
+    max_y = max(args.src_y, args.tgt_y)
+    if min < 0 or max_x >= length or max_y >= len(args.maze_rows):
+        raise Exception("src/tgt out of maze bounds")
+
+    for s in args.maze_rows:
+        if len(s) != length:
+            raise Exception("Maze not rectangular")
+        row = []
+        for char in s:
+            if char != 0 and char != 1:
+                raise Exception("Illegal maze")
+            row.append(int(char))
+
+        matrix.append(row)
+
+    return matrix
+
+
+def build_graph(matrix):
+    """
+    constructs graph representation of maze from 2d-array
+    --------------
+    Parameters:
+    matrix: 2d int array representing maze
+    --------------
+    Returns:
+    Graph object of maze, with each cell being a node and an undirected edge existing between
+    any two cells that are connected in the up/down/left/right direction. Blocked cells
+    cannot be connected to any cells
+    """
+    rows = len(matrix)
+    columns = len(matrix[0])
+    # convention of (0, 0) in the top left corner and (columns, rows) in the bottom right corner
+    g = Graph(rows * columns)
+
+    # keeps track of the current vertex
+    vertex = 0
+    for y in range(rows):
+        for x in range(columns):
+            # determine whether or not to add edges for a particular vertex
+            if matrix[y][x] = 0:
+                # scan the right side of the vertex for an edge
+                if x + 1 < columns:
+                    if matrix[y][x + 1] == 0:
+                        g.addEdge(vertex, vertex + 1)
+                # scan the left side of the vertex for an edge
+                if x - 1 >= 0:
+                    if matrix[y][x - 1] == 0:
+                        g.addEdge(vertex, vertex - 1)
+                # scan below the vertex for an edge
+                if y + 1 < rows:
+                    if matrix[y + 1][x] == 0:
+                        g.addEdge(vertex, vertex + columns)
+                # scan above the vertex for an edge
+                if y - 1 >= 0:
+                    if matrix[y - 1][x] == 0:
+                        g.addEdge(vertex, vertex - columns)
+            vertex += 1
+
+    return g
 
 
 def solve_maze():
     """
-    solves maze recursively
+    solves maze with breadth first search
     """
     pass
 
 
-def draw_soluion():
+def draw_solution():
     """
     draws out solution maze using pyplot
     """
@@ -71,7 +145,11 @@ def generate_new_maze():
 
 if __name__ == "__main__":
     args = parse_args()
-    #testing
+    # testing
+    # ./maze_solver.py
+    # python3 -c 'import maze_solver; maze_solver.parse_args()' 0 0 0 0 01010 00000
+
+    # delete the stuff below later
 
     # length of first row (all rows should have the same length)
     length = len(args.maze_rows[0])
