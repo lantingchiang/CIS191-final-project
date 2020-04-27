fc9b3a1833deafdaeeb46d0468f5997c46120c56
ph163k8 <30556772+ph163k8@users.noreply.github.com>
2020-04-26 21:20:57 -0400

    Add files via upload

--- maze_solver.py
+++ maze_solver.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 import argparse
-from graph import Graph 
+from graph import Graph
+from collections import deque
+import tkinter as tk
+import turtle
 
 
 def parse_args():
@@ -29,7 +32,9 @@ def parse_args():
     parser.add_argument("src_y", type=int, help="y coordinate of starting point in maze")
     parser.add_argument("tgt_x", type=int, help="x coordinate of target end point in maze")
     parser.add_argument("tgt_y", type=int, help="y coordinate of target end point in maze")
-    parser.add_argument("maze_rows", nargs='+', help="each row of the maze, separated by space, only 0 1 allowed")
+    parser.add_argument(
+        "maze_rows", nargs="+", help="each row of the maze, separated by space, only 0 1 allowed"
+    )
 
     args = parser.parse_args()
     return args
@@ -51,10 +56,10 @@ def construct_maze(args):
     length = len(args.maze_rows[0])
 
     # check for src and tgt in bounds
-    min = min(args.src_x, args.src_y, args.tgt_x, args.tgt_y)
+    min_of_all = min(args.src_x, args.src_y, args.tgt_x, args.tgt_y)
     max_x = max(args.src_x, args.tgt_x)
     max_y = max(args.src_y, args.tgt_y)
-    if min < 0 or max_x >= length or max_y >= len(args.maze_rows):
+    if min_of_all < 0 or max_x >= length or max_y >= len(args.maze_rows):
         raise Exception("src/tgt out of maze bounds")
 
     for s in args.maze_rows:
@@ -62,7 +67,7 @@ def construct_maze(args):
             raise Exception("Maze not rectangular")
         row = []
         for char in s:
-            if char != 0 and char != 1:
+            if char != "0" and char != "1":
                 raise Exception("Illegal maze")
             row.append(int(char))
 
@@ -93,7 +98,7 @@ def build_graph(matrix):
     for y in range(rows):
         for x in range(columns):
             # determine whether or not to add edges for a particular vertex
-            if matrix[y][x] = 0:
+            if matrix[y][x] == 0:
                 # scan the right side of the vertex for an edge
                 if x + 1 < columns:
                     if matrix[y][x + 1] == 0:
@@ -114,54 +119,144 @@ def build_graph(matrix):
 
     return g
 
-    """
-    matrix = [] 
-    for s in args.maze_rows:
-        row = []
-        for char in s:
-            if char != 0 and char != 1:
-                raise Exception("Illegal maze")
-            row.append(int(char))
 
-        matrix.append(row)
-
-    return matrix
-
-
-def build_graph(matrix):
-    """
-    constructs graph representation of maze from 2d-array
-    --------------
-    Parameters:
-    matrix: 2d int array representing maze
-    --------------
-    Returns:
-    Graph object of maze, with each cell being a node and an undirected edge existing between
-    any two cells that are connected in the up/down/left/right direction. Blocked cells
-    cannot be connected to any cells
-    """
-
-
-
-def solve_maze():
+def solve_maze(g, matrix, src_x, src_y, tgt_x, tgt_y):
     """
     solves maze with breadth first search
+    -----------
+    Parameters:
+    g: Graph
+    matrix: 2d int array representing maze
+    src_x: x coordinate of starting point in maze
+    src_y: y coordinate of starting point in maze
+    tgt_x: x coordinate of target end point in maze
+    tgt_y: y coordinate of target end point in maze
+    -----------
+    Returns:
+    list of 2-element tuples, i.e. coordinates, (x, y) along path from source to target
     """
-    pass
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    # convert source and target coordinates to vertices
+    s = (src_y * columns) + src_x
+    t = (tgt_y * columns) + tgt_x
+
+    # breadth first search with discovered and parent arrays
+    discovered = [False] * g.getSize()
+    parent = [None] * g.getSize()
+    d = deque()
+    d.append(s)
+    discovered[s] = True
+    # main loop of BFS, run from source vertex
+    while len(d) != 0:
+        v = d.popleft()
+        for u in g.neighbors(v):
+            if (not discovered[u]):
+                discovered[u] = True
+                d.append(u)
+                parent[u] = v
+    
+    ##########
+    print(parent) # testing, buggy parent array?
+    ##########
+
+    # get the solution path by traversing the parent pointers from target
+    path = []
+    curr = t
+    tgt = (tgt_x, tgt_y)
+    path.append(tgt)
+    while curr != s:
+        # no path exists so return an empty list
+        if parent[curr] is None:
+            return []
+        # convert vertex into a coordinate and append it to the solution path
+        x = parent[curr] % columns
+        coordinate = (x, int((parent[curr] - x) / columns))
+        print(coordinate)
+        path.append(coordinate)
+        curr = parent[curr]
+    # reverse the path since it starts with target and traces back to source
+    path.reverse()
+
+    return path
 
 
-def draw_solution():
+def draw_solution(matrix, path):
     """
-    draws out solution maze using pyplot
+    draws out solution maze
+    -----------
+    Parameters:
+    matrix: 2d int array
+    path: list of coordinates (tuples) along path from source to target
     """
-    pass
+    rows = len(matrix)
+    cols = len(matrix[0])
+    # width of each cell drawn; window is a bit larger than 600 * 600
+    width = 600 / max(rows, cols)
+
+    # DRAW MAZE
+    turtle.tracer(0)  # turnoff animation
+    turtle.hideturtle()
+    for r in range(rows):
+        for c in range(cols):
+            x_pos = -300 + width * c
+            y_pos = 300 - width * r
+            turtle.penup()  # doesn't draw when moving pen
+            turtle.goto(x_pos, y_pos)
+            turtle.pendown()
+
+            # draw filled shape if cell is blocked
+            if matrix[r][c] == 1:
+                turtle.fillcolor("black")
+                turtle.begin_fill()
+            # draw square
+            for i in range(4):
+                turtle.forward(width)
+                turtle.right(90)
+            turtle.end_fill()
+    turtle.update()  # show drawings
+
+    # DRAW PATH
+    # start at middle of first cell in path
+    turtle.tracer(1, 10)
+    turtle.penup()
+    turtle.goto(-300 + width * path[0][0] + width / 2, 300 - width * path[0][1] - width / 2)
+    turtle.pendown()
+    turtle.pencolor("orange")
+    turtle.pensize(10)
+    turtle.circle(10)  # draw starting point
+    for i in range(1, len(path) - 1):
+        # draw until middle of next cell
+        turtle.goto(-300 + width * path[i][0] + width / 2, 300 - width * path[i][1] - width / 2)
+
+    turtle.circle(10)  # draw end point
+
+    tk.mainloop()  # needed for canvas window
 
 
-def print_solution():
+def print_solution(coords):
     """
     prints out coordinates of solution path in command line
+    ---------------
+    Parameters:
+    coords: two-int-tuple list
+    list of coordinates along path from source to target
     """
-    pass
+    # original code
+    #string = ""
+    #for i in range(len(coords) - 1):
+        #string += coords[i] + "->"
+    #string += coords[len(coords) - 1]
+
+    string = "->".join(map(str, coords))
+
+    #for i in range(len(coords) - 1):
+        #string += "->".join(map(str, coords))
+    #string += map(str, coords)
+    #string += '->'.join(str(x) for x in coords)
+
+    print(string)
 
 
 def generate_new_maze():
@@ -172,32 +267,33 @@ def generate_new_maze():
 
 
 if __name__ == "__main__":
+    """
+    Call methods one by one to read in args, build maze and solve maze
+    """
     args = parse_args()
+
+    x1 = args.src_x
+    y1 = args.src_y
+    x2 = args.tgt_x
+    y2 = args.tgt_y
+
+    maze_matrix = construct_maze(args)
+    print(maze_matrix)
+
+    graph = build_graph(maze_matrix)
+
+    path = solve_maze(graph, maze_matrix, x1, y1, x2, y2)
+
+    print(path) # for testing purposes
+
+    print_solution(path)
+
+    path = [(1, 1), (2, 1), (2, 2), (3, 2)]  # dummy path for testing
+    draw_solution(maze_matrix, path)
+
+    # generate_new_maze()
+
     # testing
-    # ./maze_solver.py
+    # ./maze_solver.py 0 1 1 3 0000 0000 0000 0000 0000
+    # ./maze_solver.py 1 1 1 9 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0011 1111 1111
     # python3 -c 'import maze_solver; maze_solver.parse_args()' 0 0 0 0 01010 00000
-
-    # delete the stuff below later
-
-    # length of first row (all rows should have the same length)
-    length = len(args.maze_rows[0])
-    # check for src and tgt in bounds
-    min = min(args.src_x, args.src_y, args.tgt_x, args.tgt_y)
-    max = max(args.src_x, args.src_y, args.tgt_x, args.tgt_y)
-    if min < 0 or max >= len(args.maze_rows):
-        print("Error: src/tgt out of maze bounds")
-        # quit
-
-    for row in args.maze_rows:
-        if len(row) != length:
-            print("Error: Maze not rectangular")
-            # quit
-        for char in row:
-            if char != '0' and char != '1':
-                print("Error: Input consists of characters that are not 0's or 1's")
-                # quit
-        # row is a string in the list, char is a char in the string row
-        # create graph char-by-char, row-by-row
-
-    print(args.src_x)
-    print(args.maze_rows)
